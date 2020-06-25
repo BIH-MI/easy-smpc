fe8633f90da379ae478cf77cd54da382e5c33e5f
Tobias Kussel <kussel@cbs.tu-darmstadt.de>
2020-06-25 08:21:47 +0200

    Rest Server

---
+++ java/src/main/java/de/tu_darmstadt/cbs/covidapp/RestHistogramEndpoint.java
@@ -0,0 +1,51 @@
+package de.tu_darmstadt.cbs.covidapp;
+
+import net.freeutils.httpserver.HTTPServer.Context;
+import net.freeutils.httpserver.HTTPServer.Request;
+import net.freeutils.httpserver.HTTPServer.Response;
+import net.freeutils.httpserver.HTTPServer.ContextHandler;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class RestHistogramEndpoint implements ContextHandler {
+    private CovidApp app;
+
+    public RestHistogramEndpoint(CovidApp app) {
+        this.app = app;
+    }
+
+    public int serve(Request req, Response resp) {
+        Map<String, String> params;
+        try {
+            params = req.getParams();
+
+            Iterator it = params.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry p = (Map.Entry) it.next();
+                System.out.println(p.getKey() + " - " + p.getValue());
+                it.remove();
+            }
+            String responseMsg = app.getResultMessage();
+            resp.getHeaders().add("Content-Type", "text/plain");
+            if (responseMsg == "NotReadyYet") {
+                resp.getHeaders().add("Retry-After", "60");
+                resp.getHeaders().add("Connection", "close");
+                try {
+                    resp.send(503, "The result is not yet available. Try again later.\n\r");
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else {
+                resp.getHeaders().add("Connection", "close");
+                resp.send(200, app.getResultMessage() + "\r\n");
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
---
+++ java/src/main/java/de/tu_darmstadt/cbs/covidapp/RestServer.java
@@ -0,0 +1,54 @@
+package de.tu_darmstadt.cbs.covidapp;
+
+import net.freeutils.httpserver.HTTPServer;
+import net.freeutils.httpserver.HTTPServer.VirtualHost;
+
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class RestServer {
+    private int port;
+    private File keyStore;
+    private String keyStorePwd;
+    private HTTPServer server;
+    private SSLContext context;
+    private RestHistogramEndpoint endpoints;
+
+    public RestServer(CovidApp app, Configuration config)
+            throws KeyStoreException, NoSuchAlgorithmException, CertificateException, FileNotFoundException,
+            IOException, UnrecoverableKeyException, KeyManagementException {
+        port = config.restPort;
+        server = new HTTPServer(port);
+
+        VirtualHost host = server.getVirtualHost(null); // default virtual host
+        host.addContext(config.restAddressPrefix + "histogram", new RestHistogramEndpoint(app), "GET");
+        KeyStore ks = KeyStore.getInstance("jks");
+        ks.load(new FileInputStream(config.keyStore), config.keyStorePwd.toCharArray());
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(ks, config.keyStorePwd.toCharArray());
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(kmf.getKeyManagers(), null, null);
+        server.setServerSocketFactory(sslContext.getServerSocketFactory());
+    }
+
+    public void run() throws IOException {
+        server.start();
+    }
+
+    public void stop() {
+        server.stop();
+    }
+
+}
